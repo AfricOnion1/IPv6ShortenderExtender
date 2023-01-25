b378897ef46dc08ef745c473a4b6638196547ae7
Leon Kušić <leonkusic2005@gmail.com>
2023-01-25 22:55:45 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace IPv6ShortenderExtender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.OutputLabel = new System.Windows.Forms.Label();
+            this.CopyOutputButton = new System.Windows.Forms.Button();
+            this.DeleteInputButton = new System.Windows.Forms.Button();
+            this.ErrorMessageLabel = new System.Windows.Forms.Label();
+            this.SwitchModeLabelLeft = new System.Windows.Forms.Label();
+            this.SwitchModeButton = new System.Windows.Forms.Button();
+            this.SwitchModeLabelRight = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTextBox.Location = new System.Drawing.Point(12, 73);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(560, 31);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F);
+            this.OutputTextBox.Location = new System.Drawing.Point(12, 239);
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(560, 31);
+            this.OutputTextBox.TabIndex = 1;
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.ConvertButton.ForeColor = System.Drawing.Color.Black;
+            this.ConvertButton.Location = new System.Drawing.Point(217, 141);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(150, 60);
+            this.ConvertButton.TabIndex = 2;
+            this.ConvertButton.Text = "SKRATI";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputLabel.Location = new System.Drawing.Point(12, 55);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(105, 15);
+            this.InputLabel.TabIndex = 3;
+            this.InputLabel.Text = "Upiši IPv6 adresu:";
+            // 
+            // OutputLabel
+            // 
+            this.OutputLabel.AutoSize = true;
+            this.OutputLabel.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.OutputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.OutputLabel.Location = new System.Drawing.Point(12, 221);
+            this.OutputLabel.Name = "OutputLabel";
+            this.OutputLabel.Size = new System.Drawing.Size(129, 15);
+            this.OutputLabel.TabIndex = 4;
+            this.OutputLabel.Text = "Skraćena IPv6 adresa:";
+            // 
+            // CopyOutputButton
+            // 
+            this.CopyOutputButton.Location = new System.Drawing.Point(496, 277);
+            this.CopyOutputButton.Name = "CopyOutputButton";
+            this.CopyOutputButton.Size = new System.Drawing.Size(75, 23);
+            this.CopyOutputButton.TabIndex = 5;
+            this.CopyOutputButton.Text = "Kopiraj";
+            this.CopyOutputButton.UseVisualStyleBackColor = true;
+            this.CopyOutputButton.Click += new System.EventHandler(this.CopyOutputButton_Click);
+            // 
+            // DeleteInputButton
+            // 
+            this.DeleteInputButton.Location = new System.Drawing.Point(497, 110);
+            this.DeleteInputButton.Name = "DeleteInputButton";
+            this.DeleteInputButton.Size = new System.Drawing.Size(75, 23);
+            this.DeleteInputButton.TabIndex = 6;
+            this.DeleteInputButton.Text = "Izbriši";
+            this.DeleteInputButton.UseVisualStyleBackColor = true;
+            this.DeleteInputButton.Click += new System.EventHandler(this.DeleteInputButton_Click);
+            // 
+            // ErrorMessageLabel
+            // 
+            this.ErrorMessageLabel.AutoSize = true;
+            this.ErrorMessageLabel.ForeColor = System.Drawing.Color.Red;
+            this.ErrorMessageLabel.Location = new System.Drawing.Point(12, 110);
+            this.ErrorMessageLabel.Name = "ErrorMessageLabel";
+            this.ErrorMessageLabel.Size = new System.Drawing.Size(99, 13);
+            this.ErrorMessageLabel.TabIndex = 7;
+            this.ErrorMessageLabel.Text = "Error Message Text";
+            this.ErrorMessageLabel.Visible = false;
+            // 
+            // SwitchModeLabelLeft
+            // 
+            this.SwitchModeLabelLeft.AutoSize = true;
+            this.SwitchModeLabelLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.SwitchModeLabelLeft.Location = new System.Drawing.Point(230, 23);
+            this.SwitchModeLabelLeft.Name = "SwitchModeLabelLeft";
+            this.SwitchModeLabelLeft.Size = new System.Drawing.Size(42, 17);
+            this.SwitchModeLabelLeft.TabIndex = 8;
+            this.SwitchModeLabelLeft.Text = "Duga";
+            // 
+            // SwitchModeButton
+            // 
+            this.SwitchModeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.SwitchModeButton.Location = new System.Drawing.Point(276, 15);
+            this.SwitchModeButton.Name = "SwitchModeButton";
+            this.SwitchModeButton.Size = new System.Drawing.Size(32, 32);
+            this.SwitchModeButton.TabIndex = 9;
+            this.SwitchModeButton.Text = "→";
+            this.SwitchModeButton.UseVisualStyleBackColor = true;
+            this.SwitchModeButton.Click += new System.EventHandler(this.SwitchModeButton_Click);
+            // 
+            // SwitchModeLabelRight
+            // 
+            this.SwitchModeLabelRight.AutoSize = true;
+            this.SwitchModeLabelRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.SwitchModeLabelRight.Location = new System.Drawing.Point(312, 23);
+            this.SwitchModeLabelRight.Name = "SwitchModeLabelRight";
+            this.SwitchModeLabelRight.Size = new System.Drawing.Size(49, 17);
+            this.SwitchModeLabelRight.TabIndex = 10;
+            this.SwitchModeLabelRight.Text = "Kratka";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 311);
+            this.Controls.Add(this.SwitchModeLabelRight);
+            this.Controls.Add(this.SwitchModeButton);
+            this.Controls.Add(this.SwitchModeLabelLeft);
+            this.Controls.Add(this.ErrorMessageLabel);
+            this.Controls.Add(this.DeleteInputButton);
+            this.Controls.Add(this.CopyOutputButton);
+            this.Controls.Add(this.OutputLabel);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.ConvertButton);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.Label OutputLabel;
+        private System.Windows.Forms.Button CopyOutputButton;
+        private System.Windows.Forms.Button DeleteInputButton;
+        private System.Windows.Forms.Label ErrorMessageLabel;
+        private System.Windows.Forms.Label SwitchModeLabelLeft;
+        private System.Windows.Forms.Button SwitchModeButton;
+        private System.Windows.Forms.Label SwitchModeLabelRight;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Net;
+
+namespace IPv6ShortenderExtender
+{
+    public partial class Form1 : Form
+    {
+        // selecta razmake
+        //private static readonly Regex sWhitespace = new Regex(@"\s+");
+        // stari regex koji radi ako je manje od 4 nule u segmentu
+        //private static readonly Regex sLeadingZeros = new Regex(@"((?<=(^|:))0{1,3})(?!($|:))");
+
+        // selecta prve 3 nule svakog segmenta
+        private static readonly Regex sLeadingZeros = new Regex(@"((?<=(^|:))0{1,3})");
+        // selecta same nule
+        private static readonly Regex sSoloZeros = new Regex(@"(:{1,2})?(?<=(^|:))0(:{1,2})?");
+
+        // selecta ::
+        private static readonly Regex sDoubleColon = new Regex(@"::");
+        // selecta sve nizove dvotocki (:) koji su duzi od ::
+        private static readonly Regex sMultipleColon = new Regex(@":{3,}");
+
+        private bool shortenMode = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            bool isIPv6AddressValid = IsIPv6AddressValid(InputTextBox.Text, out string errorMessage);
+
+            if (!isIPv6AddressValid)
+            {
+                ErrorMessageLabel.Text = errorMessage;
+                ErrorMessageLabel.Visible = true;
+                OutputTextBox.Text = "";
+                return;
+            }
+
+            ErrorMessageLabel.Visible = false;
+
+            if (shortenMode) OutputTextBox.Text = ShortenIPv6(InputTextBox.Text);
+            else OutputTextBox.Text = ExpandIPv6(InputTextBox.Text);
+        }
+
+        private string ShortenIPv6(string IPv6Address)
+        {
+            IPv6Address = ExpandIPv6(IPv6Address);
+
+            IPv6Address = sLeadingZeros.Replace(IPv6Address, "");
+            IPv6Address = sSoloZeros.Replace(IPv6Address, "s");
+
+            string longestSequence = FindLongestSequence(IPv6Address, 's');
+
+            if (longestSequence != "")
+            {
+                Regex sLongestSequence = new Regex(longestSequence);
+                IPv6Address = sLongestSequence.Replace(IPv6Address, "::", 1);
+            }
+
+            IPv6Address = AddZerosBack(IPv6Address, 's');
+
+            return IPv6Address.ToLower();
+        }
+
+        private string FindLongestSequence(string input, char c)
+        {
+            int longestSequence = 0;
+            int currentSequence = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == c)
+                {
+                    currentSequence++;
+                    longestSequence = Math.Max(longestSequence, currentSequence);
+                }
+                else currentSequence = 0;
+            }
+
+            return new string(c, longestSequence);
+        }
+
+        private string AddZerosBack(string input, char c)
+        {
+            string result = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == c && (i > 0 ? result[result.Length - 1] != ':' : true)) result += ":0:";
+                else if (input[i] == c) result += "0:";
+                else result += input[i];
+            }
+
+            if (input.Substring(0, 2) == "::") return "::" + result.Trim(':');
+
+            if (input.Substring(input.Length - 2, 2) == "::" ) return result.Trim(':') + "::";
+
+            return result.Trim(':');
+        }
+
+        private string ExpandIPv6(string IPv6Address)
+        {
+            string[] hexGroups = IPv6Address.Split(':');
+            string[] expandedHexGroups = new string[8];
+
+            int i = 0;
+            // za slucajeve di je :: na pocetku/kraju ili je samo ::
+            int excessEmptySegments = hexGroups.Where(s => s == "").Count() - 1;
+
+            for (i = 0; i < excessEmptySegments; i++)
+            {
+                int excessSegmentIndex = Array.IndexOf(hexGroups, "");
+                hexGroups = hexGroups.Where((s, idx) => idx != excessSegmentIndex).ToArray();
+            }
+
+            int missingSegmentIndex = Array.IndexOf(hexGroups, "");
+
+            if (missingSegmentIndex != -1)
+            {
+                foreach (string s in hexGroups) Console.WriteLine(s);
+
+                int missingSegmentsAmount = 9 - hexGroups.Length;
+
+                string[] missingSegments = Enumerable.Repeat("0000", missingSegmentsAmount).ToArray();
+
+                string[] firstArray = hexGroups.Take(missingSegmentIndex).ToArray();
+                string[] secondArray = hexGroups.Skip(missingSegmentIndex + 1).ToArray();
+
+                hexGroups = firstArray.Concat(missingSegments).Concat(secondArray).ToArray();
+            }
+
+            i = 0;
+            foreach (string s in hexGroups)
+            {
+                if (s.Length > 0 && s.Length < 4) expandedHexGroups[i] = new string('0', 4 - s.Length) + s;
+                else expandedHexGroups[i] = hexGroups[i];
+
+                i++;
+            }
+
+            return string.Join(":", expandedHexGroups);
+        }
+
+        private void DeleteInputButton_Click(object sender, EventArgs e)
+        {
+            InputTextBox.Text = "";
+            OutputTextBox.Text = "";
+        }
+
+        private void CopyOutputButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(OutputTextBox.Text);
+        }
+
+        private bool IsIPv6AddressValid(string IPv6Address, out string errorMessage)
+        {
+            errorMessage = null;
+
+            int IPv6AddressLength = IPv6Address.Length;
+
+            char[] allowedChars =
+                { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', ':' };
+
+            IPv6Address = IPv6Address.ToLower();
+            string[] hexGroups = IPv6Address.Split(':');
+
+            MatchCollection doubleColonMatches = sDoubleColon.Matches(IPv6Address);
+
+            bool doubleColonAtEnds = (IPv6Address[0] == ':' && IPv6Address[1] == ':') ||
+                (IPv6Address[IPv6AddressLength - 1] == ':' && IPv6Address[IPv6AddressLength - 2] == ':');
+
+            if (!IPv6Address.All(c => allowedChars.Contains(c)))
+            {
+                errorMessage = "IPv6 adresa ne sadrži pravilne heksadekadske znamenke!";
+                return false;
+            }
+
+            foreach (string s in hexGroups)
+            {
+                if (s.Length > 4)
+                {
+                    errorMessage = "Neki segmenti su pre dugi!";
+                    return false;
+                }
+            }
+
+            if ((hexGroups.Length > 8 && !doubleColonAtEnds) || (hexGroups.Length > 9 && doubleColonAtEnds))
+            {
+                errorMessage = "IPv6 adresa ima previše segmenata!";
+                return false;
+            }
+
+            if (doubleColonMatches.Count == 0 && hexGroups.Length < 8)
+            {
+                errorMessage = "IPv6 adresa ima premalo segmenata!";
+                return false;
+            }
+            
+            if ((IPv6Address[0] == ':' && IPv6Address[1] != ':') ||
+                (IPv6Address[IPv6AddressLength - 1] == ':' && IPv6Address[IPv6AddressLength - 2] != ':'))
+            {
+                errorMessage = "IPv6 adresa ne može započinjati ili završavati sa \":\"!";
+                return false;
+            }
+
+            if (sMultipleColon.IsMatch(IPv6Address))
+            {
+                errorMessage = "Ne može biti više od 2 ':' za redom!";
+                return false;
+            }
+
+            if (doubleColonMatches.Count > 1)
+            {
+                errorMessage = "Može biti samo 1 \"::\"!";
+                return false;
+            }
+
+            return true;
+        }
+
+        private void SwitchModeButton_Click(object sender, EventArgs e)
+        {
+            // duga <=> kratka
+            shortenMode = !shortenMode;
+
+            if (shortenMode)
+            {
+                SwitchModeLabelLeft.Text = "Duga";
+                SwitchModeLabelRight.Text = "Kratka";
+                OutputLabel.Text = "Skraćena IPv6 adresa:";
+                ConvertButton.Text = "SKRATI";
+                return;
+            }
+
+            SwitchModeLabelLeft.Text = "Kratka";
+            SwitchModeLabelRight.Text = "Duga";
+            OutputLabel.Text = "Produžena IPv6 adresa:";
+            ConvertButton.Text = "PRODUŽI";
+            return;
+        }
+    }
+}
